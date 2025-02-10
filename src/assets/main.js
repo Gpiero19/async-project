6a242f8809e406079c0be8669deb937d487eeba1
Gian Piero Canevari <canevarigian@gmail.com>
2025-02-10 23:28:02 +0100

    content

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,7 @@
-const url = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UCJ5v_MCY6GNUBTO8-D3XoAg&filter=videos_latest&hl=en&gl=US';
+const API = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UCJ5v_MCY6GNUBTO8-D3XoAg&filter=videos_latest&hl=en&gl=US';
+
+const content = null || document.getElementById('content');
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -7,10 +10,32 @@ const options = {
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
+async function fetchData(urlAPI) {
+	const response = await fetch(urlAPI, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+	    const videos = await fetchData(API);
+	    console.log(videos);
+        let view = `
+        ${videos.items.map(video => `
+            <div class="group relative">
+            <div
+                class="w-full bg-black aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+                </h3>
+            </div>
+            </div>
+            `).slice(0,4).join('')}
+            `;
 } catch (error) {
 	console.error(error);
-}
+}});
